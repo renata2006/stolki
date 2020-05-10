5f783f39bb7426e63e255385484cbc0783176da4
Tomasz Grochocki <tomasz.grochocki06@gmail.com>
2020-05-10 22:49:20 +0200

    Gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,30 @@ const cleanCSS = require("gulp-clean-css");
 const concat = require("gulp-concat");
 const uglify = require("gulp-uglify-es").default;
 
-gulp.task("minify-css", () => {
+function styles() {
   return gulp
     .src("styles/*.css")
     .pipe(cleanCSS({ compatibility: "ie8" }))
     .pipe(concat("style.min.css"))
     .pipe(gulp.dest("dist"));
-});
+}
 
-gulp.task("minify-js", () => {
-  gulp
+function scripts() {
+  return gulp
     .src("js/*.js")
     .pipe(uglify())
     .pipe(concat("script.min.js"))
     .pipe(gulp.dest("dist"));
-});
+}
 
-gulp.task("build", ["minify-css", "minify-js"]);
+function watch() {
+  gulp.watch("styles/*.css", styles);
+  gulp.watch("js/*.js", scripts);
+}
+
+var build = gulp.series(gulp.parallel(styles, scripts));
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watch = watch;
+exports.build = build;
