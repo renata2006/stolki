5cc12fcee411f4ff90e8a37e933ab76147c2fb53
Tomasz Grochocki <tomasz.grochocki06@gmail.com>
2020-05-13 12:07:56 +0200

    Download link

--- js/gallery.js
+++ js/gallery.js
@@ -2,11 +2,13 @@ const modal = document.querySelector(".modal");
 const span = modal.querySelector("span");
 const content = modal.querySelector(".modal-content");
 const nav = document.querySelector(".main-navigation");
+const download = document.querySelector(".download > a");
 let showIndex = 0;
 let clone;
 let images;
 
 function showModal(arr, image, index) {
+  console.log(arr);
   images = arr;
   showIndex = index;
   modal.style.display = "block";
@@ -14,6 +16,8 @@ function showModal(arr, image, index) {
   content.appendChild(clone);
   updateText();
   nav.style.display = "none";
+  console.log(image);
+  download.setAttribute("href", addLink(image));
 }
 function closeModal() {
   content.removeChild(clone);
@@ -51,4 +55,8 @@ function setIndex(val) {
 
 function updateText() {
   span.innerHTML = `${showIndex + 1} / ${images.length}`;
+}
+
+function addLink(image) {
+  return image.getAttribute("src");
 }
