105f1f8b07ec79b7365e86fb158b647f00b36ecb
Tomasz Grochocki <tgrochocki@future-processing.com>
2021-12-18 23:11:51 +0100

    recaptcha

--- src/email.ts
+++ src/email.ts
@@ -4,15 +4,33 @@ init("user_R1UO7zkQn9IkFmSpudSnW");
 const myForm = document.querySelector("form");
 const button = document.querySelector(".email-send");
 
+declare var grecaptcha: any;
+
 export function initEmail() {
-  button.addEventListener("click", () => {
-    sendForm("service_1rsve0j", "template_m0rgnrs", myForm).then(
-      function (response) {
-        console.log("SUCCESS!", response.status, response.text);
-      },
-      function (error) {
-        console.log("FAILED...", error);
-      }
-    );
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    grecaptcha.ready(function () {
+      grecaptcha
+        .execute("6LcNAbEdAAAAABpkFzI6QqnacoTQpqTUbY-i-lUF", {
+          action: "submit",
+        })
+        .then(function (token) {
+          var input = document.createElement("input");
+          input.setAttribute("type", "hidden");
+          input.setAttribute("name", "g-recaptcha-response");
+          input.setAttribute("value", token);
+          myForm.append(input);
+          // Add your logic to submit to your backend server here.
+
+          sendForm("service_1rsve0j", "template_m0rgnrs", myForm).then(
+            function (response) {
+              console.log("SUCCESS!", response.status, response.text);
+            },
+            function (error) {
+              console.log("FAILED...", error);
+            }
+          );
+        });
+    });
   });
 }
